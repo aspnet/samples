4e93a940f0d88e88b4babd753d89503439089681
Howard Richards <dev@conficient.com>
2018-10-01 17:48:39 +0100

    Replace references to 'arrive' or 'arrival' to 'return' to be consistent

--- samples/aspnetcore/blazor/FlightFinder/FlightFinder.Shared/FlightSegment.cs
+++ samples/aspnetcore/blazor/FlightFinder/FlightFinder.Shared/FlightSegment.cs
@@ -8,7 +8,7 @@ namespace FlightFinder.Shared
         public string FromAirportCode { get; set; }
         public string ToAirportCode { get; set; }
         public DateTime DepartureTime { get; set; }
-        public DateTime ArrivalTime { get; set; }
+        public DateTime ReturnTime { get; set; }
         public double DurationHours { get; set; }
         public TicketClass TicketClass { get; set; }
     }
