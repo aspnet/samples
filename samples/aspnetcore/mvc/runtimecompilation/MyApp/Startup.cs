0bb5d5837f080cb3d174dba623711891bbd286d4
Pranav K <prkrishn@hotmail.com>
2019-10-03 07:38:25 -0700

    Add a sample showing runtime compilation working in dev

---
+++ samples/aspnetcore/mvc/runtimecompilation/MyApp/Startup.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.FileProviders;
+
+namespace MyApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            Configuration = configuration;
+            this.env = env;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public IWebHostEnvironment env { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var builder = services.AddControllersWithViews();
+#if DEBUG
+            if (env.IsDevelopment())
+            {
+                builder.AddRazorRuntimeCompilation(options =>
+                {
+                    var libraryPath = Path.GetFullPath(Path.Combine(env.ContentRootPath, "..", "MyClassLib"));
+                    options.FileProviders.Add(new PhysicalFileProvider(libraryPath));
+                });
+            }
+#endif
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
