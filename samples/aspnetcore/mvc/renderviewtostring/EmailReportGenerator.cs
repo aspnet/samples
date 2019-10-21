77b57686d474c03376d0f7ab7905fc194b6dae0e
Pranav K <prkrishn@hotmail.com>
2019-10-21 11:27:33 -0700

    Add a sample to demonstrate generating views outside of the MVC pipeline
    This is a port of the older sample located in https://github.com/aspnet/Entropy/tree/master/samples/Mvc.RenderViewToString

---
+++ samples/aspnetcore/mvc/renderviewtostring/EmailReportGenerator.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace Mvc.RenderViewToString
+{
+    public class EmailReportGenerator
+    {
+        public string GenerateReport(int userData1, int userData2)
+        {
+            return "A user specific report based on values " + userData1 + " and " + userData2;
+        }
+    }
+}
---
+++ samples/aspnetcore/mvc/renderviewtostring/EmailViewModel.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace Mvc.RenderViewToString
+{
+    public class EmailViewModel
+    {
+        public string UserName { get; set; }
+
+        public string SenderName { get; set; }
+
+        public int UserData1 { get; set; }
+
+        public int UserData2 { get; set; }
+    }
+}
