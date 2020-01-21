ca3c30ce7a516cd5d42c065bb16442e7b098adfa
Pranav K <prkrishn@hotmail.com>
2020-01-21 10:13:18 -0800

    Update Startup.cs

--- samples/aspnetcore/mvc/runtimecompilation/MyApp/Startup.cs
+++ samples/aspnetcore/mvc/runtimecompilation/MyApp/Startup.cs
@@ -42,7 +42,7 @@ namespace MyApp
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app)
         {
             if (env.IsDevelopment())
             {
