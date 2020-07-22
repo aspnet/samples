03ab9490f2f14b9f26844e00c9cbaa57b59367de
Arnab Roy Chowdhury <arnab-roy-chowdhury@outlook.com>
2020-07-22 18:15:28 +0530

    Use 'DeepEqual' nuget package to compact the unit test.

--- samples/aspnetcore/mvc/dataaccess/DataAccess.Test/StudentControllerTests.cs
+++ samples/aspnetcore/mvc/dataaccess/DataAccess.Test/StudentControllerTests.cs
@@ -1,6 +1,7 @@
 using DataAccess.WebApplication.Controllers;
 using DataAccess.WebApplication.Data;
 using DataAccess.WebApplication.Models;
+using DeepEqual.Syntax;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using NUnit.Framework;
@@ -12,6 +13,7 @@ namespace DataAccess.Test
     {
         private StudentController _studentController;
         private StudentContext _studentContext;
+        private IEnumerable<Student> _students;
 
         [SetUp]
         public void Setup()
@@ -33,23 +35,7 @@ namespace DataAccess.Test
             IActionResult indexResult = _studentController.Index();
             Assert.IsInstanceOf<ViewResult>(indexResult);
             var students = (indexResult as ViewResult).Model as List<Student>;
-            Assert.IsNotNull(students);
-
-            Assert.AreEqual(1, students[0].Id);
-            Assert.AreEqual("Stu1", students[0].Name);
-            Assert.AreEqual("Sub1", students[0].Subject);
-
-            Assert.AreEqual(2, students[1].Id);
-            Assert.AreEqual("Stu2", students[1].Name);
-            Assert.AreEqual("Sub2", students[1].Subject);
-
-            Assert.AreEqual(3, students[2].Id);
-            Assert.AreEqual("Stu3", students[2].Name);
-            Assert.AreEqual("Sub3", students[2].Subject);
-
-            Assert.AreEqual(4, students[3].Id);
-            Assert.AreEqual("Stu4", students[3].Name);
-            Assert.AreEqual("Sub4", students[3].Subject);
+            students.ShouldDeepEqual(_students);
         }
 
         [TearDown]
@@ -60,11 +46,14 @@ namespace DataAccess.Test
 
         private void PopulateData()
         {
-            _studentContext.Students.AddRange(
+            _students = new Student[]
+            {
                 new Student { Id = 1, Name = "Stu1", Subject = "Sub1" },
                 new Student { Id = 2, Name = "Stu2", Subject = "Sub2" },
                 new Student { Id = 3, Name = "Stu3", Subject = "Sub3" },
-                new Student { Id = 4, Name = "Stu4", Subject = "Sub4" });
+                new Student { Id = 4, Name = "Stu4", Subject = "Sub4" }
+            };
+            _studentContext.Students.AddRange(_students);
             _studentContext.SaveChanges();
         }
     }
