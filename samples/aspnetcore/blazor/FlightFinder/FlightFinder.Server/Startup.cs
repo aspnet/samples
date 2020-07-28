8303f50f9654af83556b636516634c56939da129
Howard Richards <dev@conficient.com>
2020-07-28 12:03:08 +0100

    Added response compression service

--- samples/aspnetcore/blazor/FlightFinder/FlightFinder.Server/Startup.cs
+++ samples/aspnetcore/blazor/FlightFinder/FlightFinder.Server/Startup.cs
@@ -17,6 +17,7 @@ namespace FlightFinder.Server
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddResponseCompression();
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
