e2899240245e44877b8462e6b333e8b260d948ea
Yong Zhao <yong.zhao@nscorp.com>
2020-09-24 15:57:10 -0400

    use MSSQLLocalDB and create demo application if not exist

--- samples/aspnet/Identity/SQLMembership-Identity-OWIN/SQLMembership-Identity-OWIN/IdentityAccount/Register.aspx.cs
+++ samples/aspnet/Identity/SQLMembership-Identity-OWIN/SQLMembership-Identity-OWIN/IdentityAccount/Register.aspx.cs
@@ -51,9 +51,30 @@ namespace SQLMembership_Identity_OWIN.IdentityAcccount
                 {
                     return reader.GetGuid(0);
                 }
-
-                return Guid.NewGuid();
+                connection.Close();
+                return createApplication(connection, "/");
             }
+        }
+
+        private Guid createApplication(SqlConnection sqlConnection, string s)
+        {
+            var appid = Guid.NewGuid();
+            var insStatement =
+                $"INSERT INTO [dbo].aspnet_Applications" +
+                $"([ApplicationName], [LoweredApplicationName], [ApplicationId], [Description]) " +
+                $"VALUES (@appName, @lowAppName, @appId, 'SQLMembership-Identity-OWIN')";
+
+            var sqlcmd = new SqlCommand(insStatement, sqlConnection);
+            sqlcmd.Connection.Open();
+
+            sqlcmd.Parameters.AddWithValue("@appId", appid);
+            sqlcmd.Parameters.AddWithValue("@appName", s);
+            sqlcmd.Parameters.AddWithValue("@lowAppName",
+                (s ?? "").ToLower());
+
+            var rc = sqlcmd.ExecuteNonQuery();
+            Console.WriteLine($"insert application returns {rc}");
+            return appid;
         }
     }
 }
---
+++ samples/aspnet/Identity/SQLMembership-Identity-OWIN/SQLMembership-Identity-OWIN/create-db.ps1
@@ -0,0 +1,7 @@
+﻿<# 
+create the required aspnetdb database 
+#>
+
+$dbfile = "${PSScriptRoot}\App_Data\aspnetdb.mdf"
+Write-Host "Creating aspnetdb on $dbfile"
+& osql -S "(localdb)\MSSQLLocalDB" -E -Q "Create DATABASE [aspnetdb] ON PRIMARY (NAME='aspnetdb', FILENAME='$dbfile')"
