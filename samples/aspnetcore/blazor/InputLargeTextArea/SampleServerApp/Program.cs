96229c4b2678f2b04185cb4cd742d50091b43fb6
Tanay Parikh <taparik@microsoft.com>
2021-08-25 10:58:03 -0700

    Trim Error Page

--- samples/aspnetcore/blazor/InputLargeTextArea/SampleServerApp/Program.cs
+++ samples/aspnetcore/blazor/InputLargeTextArea/SampleServerApp/Program.cs
@@ -12,7 +12,6 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
